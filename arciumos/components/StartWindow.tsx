0fb789557e534ae99444c61aed07410e279d0de9
Volcar144 <futuretech.coder4@gmail.com>
2026-06-24 17:22:04 +0100

    Updates

--- arciumos/components/StartWindow.tsx
+++ arciumos/components/StartWindow.tsx
@@ -10,7 +10,7 @@ export default function StartWindow() {
 
 	return (
 		<Draggable nodeRef={nodeRef}>
-			<div className="flex flex-col items-center justify-center py-2 bg-stone-100 border border-black-300 border-solid h-1/12 w-4/12 rounded-md position-absolute" ref={nodeRef}>
+			<div className="flex flex-col items-center justify-center py-2 bg-stone-100 border border-black-300 border-solid h-1/12 w-4/12 rounded-md position-absolute -translate-x-1/2 -translate-y-1/2" ref={nodeRef}>
 				<Logo />
 				<h1 className="text-2xl font-bold" ref={nodeRef}>Welcome to ArciumOS</h1>
 				<h2 className="text-xl" ref={nodeRef}>The most useful OS on the web</h2>
